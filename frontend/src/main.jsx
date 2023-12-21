a03c76fff42a356297761e93679128c1f2bcc232
Romain <98345704+Romlion@users.noreply.github.com>
2023-12-21 16:25:18 +0100

    Nabar is ready

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { GlobalContextProvider } from "./components/Context/GlobalContextProvider";
 import App from "./App";
+import NavBar from "./components/NavBar/NavBar";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/",
+    element: <NavBar />,
   },
 ]);
 
