3686bcf486cb9b260c8058bcbab35ed375b6caa8
Pierre-Adrien Gouillart <pagouillart@hotmail.fr>
2024-01-05 11:14:45 +0100

    tag reversal done

--- frontend/src/components/Profil/ProfilCard.jsx
+++ frontend/src/components/Profil/ProfilCard.jsx
@@ -4,15 +4,15 @@ import "./ProfilCard.scss";
 function ProfilCard(userProfil) {
   const { name, lastname, email, password } = userProfil;
   return (
-    <section className="profilCard">
+    <article className="profilCard">
       <img src={ProfilPic} alt="Profil" className="profilPic" />
-      <article className="profilInfo">
+      <section className="profilInfo">
         <p className="profilName">Prénom: {name}</p>
         <p className="profilLastname">Nom: {lastname}</p>
         <p className="profilEmail">E-mail: {email}</p>
         <p className="profilPassword"> Password: {password}</p>
-      </article>
-    </section>
+      </section>
+    </article>
   );
 }
 
