e1eaccaa006e8e383a8021a6adfe4d1057b6238b
Romain <98345704+Romlion@users.noreply.github.com>
2024-01-05 12:01:23 +0100

    About Page

---
+++ frontend/src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,24 @@
+import "./AboutPage.scss";
+
+function AboutPage() {
+  return (
+    <>
+      <h2>RGPD</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum, et
+        reprehenderit suscipit minima praesentium illum nihil sint quod sunt,
+        magni esse? Modi, rem quo. Cumque quaerat ullam minima provident error.
+      </p>
+
+      <h2>A Propos du site</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quia
+        perferendis eligendi rerum blanditiis doloremque dignissimos! Harum
+        excepturi, obcaecati beatae sed autem exercitationem iste labore! Modi
+        quisquam veniam aliquid dolore.
+      </p>
+    </>
+  );
+}
+
+export default AboutPage;
