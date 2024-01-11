fbad2f9a2dbd012a9a00993bb452c9d596f99f05
CYRIL DABURON <cyril.daburon@gmail.com>
2024-01-11 10:42:33 +0100

    the button become a modal

--- frontend/src/components/ReturnMainMenuButton/ReturnMainMenuButton.jsx
+++ frontend/src/components/ReturnMainMenuButton/ReturnMainMenuButton.jsx
@@ -1,42 +1,54 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import "./ReturnMainMenuButton.scss";
 
 function ReturnMainMenuButton() {
-  const [popupMessage, setPopupMessage] = useState("");
-  function HandleEvent() {
-    setPopupMessage(
-      <article className="messagePopUp">
-        Voulez-vous vraiment revenir sur l'écran d'accueil ?
-        <footer className="areYouSureButton">
-          <Link to="/" className="backtoMainMenu">
-            Oui
-          </Link>
-          <button
-            type="button"
-            className="backtoMainMenu"
-            onKeyDown={() => setPopupMessage("")}
-            onClick={() => setPopupMessage("")}
-          >
-            Non
-          </button>
-        </footer>
-      </article>
-    );
-  }
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
-    <>
-      <button
-        type="button"
-        className="backMainMenuButton"
-        onKeyDown={HandleEvent}
-        onClick={HandleEvent}
-        aria-label="Fermer la page de jeu"
-      >
-        X
-      </button>
-      {popupMessage}
-    </>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger asChild>
+        <button type="button" className="openModal">
+          X
+        </button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Portal>
+        <AlertDialog.Overlay className="AlertDialogOverlay" />
+        <AlertDialog.Content className="AlertDialogContent">
+          <AlertDialog.Title
+            className="AlertDialogTitle"
+            aria-label="Bouton pour retourner au menu principal"
+          >
+            Voulez-vous vraiment revenir au menu principal ?
+          </AlertDialog.Title>
+          <article className="mainMenuContainer">
+            <AlertDialog.Cancel asChild>
+              <button
+                type="button"
+                className="areYouSureButton"
+                aria-label="Je ne souhaite pas retourner au menu principal"
+              >
+                Non
+              </button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action asChild>
+              <button
+                type="button"
+                className="areYouSureButton"
+                onClick={handleClick}
+                aria-label="Je retourne sur le menu principal"
+              >
+                Oui
+              </button>
+            </AlertDialog.Action>
+          </article>
+        </AlertDialog.Content>
+      </AlertDialog.Portal>
+    </AlertDialog.Root>
   );
 }
 
